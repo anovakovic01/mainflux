eefacc1f735f115c418b82cf697b0dca9db3e9bf
Aleksandar Novakovic <aleksandar.novakovic@mainflux.com>
2019-04-09 23:18:01 +0200

    Fix mongo reader and writer
    Fix mongo reader and writer by updating driver version.
    
    Signed-off-by: Aleksandar Novakovic <aleksandar.novakovic@mainflux.com>

--- readers/mongodb/messages_test.go
+++ readers/mongodb/messages_test.go
@@ -22,7 +22,8 @@ import (
 	"github.com/mainflux/mainflux"
 
 	log "github.com/mainflux/mainflux/logger"
-	"github.com/mongodb/mongo-go-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const (
@@ -46,7 +47,7 @@ var (
 )
 
 func TestReadAll(t *testing.T) {
-	client, err := mongo.Connect(context.Background(), addr, nil)
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(addr))
 	require.Nil(t, err, fmt.Sprintf("Creating new MongoDB client expected to succeed: %s.\n", err))
 
 	db := client.Database(testDB)
